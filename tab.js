7aee98a91b4fbfbb064d8ac2bf4fd8f16550404b
rebecca9201 <wangmmqq@gmail.com>
2017-09-13 22:21:52 +0800

    can dynamically change tab

---
+++ tab.js
@@ -0,0 +1,59 @@
+Vue.component("tabs", {
+  template: `
+    <div>
+      <div class="tabs">
+        <ul>
+          <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+            <a href="#" @click="selectTab(tab)">{{ tab.name }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  data() {
+    return { tabs: [] };
+  },
+
+  mounted() {
+    console.log(this.$children);
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name == selectedTab.name);
+      });
+    }
+  }
+});
+
+Vue.component('tab', {
+  template: `
+    <div><slot></slot></div>
+  `,
+  props: {
+    name: { required: true },
+    selected: { default: false }
+  },
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  mounted() {
+    this.isActive = this.selected;
+  }
+});
+
+new Vue({
+  el: "#tab"
+});
